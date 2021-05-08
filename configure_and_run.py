bc3928b1d8d35d2f0ac4700e5faadc0f6b9b3bdc
Pilkevich Anton <pilkevich.aa@phystech.edu>
2021-05-08 23:13:14 +0300

    add buttom

--- configure_and_run.py
+++ configure_and_run.py
@@ -1,14 +1,12 @@
 import subprocess
-import os
 from pyngrok import ngrok
 
 def main() -> None:
     """Start the bot."""
 
     http_tunnel = ngrok.connect(5000, bind_tls=True)
-    f1 = subprocess.Popen(['python3', 'flask_server.py', http_tunnel.public_url])
-
-    f1.wait()
+    with subprocess.Popen(['python3', 'flask_server.py', http_tunnel.public_url]) as proc:
+        proc.wait()
 
 if __name__ == '__main__':
     main()
--- flask_server.py
+++ flask_server.py
@@ -1,11 +1,16 @@
 import logging
 import sys
-import requests
+
+from queue import Queue
+from threading import Thread
 from waitress import serve
-from flask import Flask, jsonify, request
+from flask import Flask, request
+
+from telegram import Bot, Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Dispatcher, MessageHandler, Filters
+
 from settings import TOKEN
-from telegram import Bot, Update, ForceReply
-from telegram.ext import Dispatcher, Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Enable logging
 logging.basicConfig(
@@ -15,8 +20,13 @@ logging.basicConfig(
 logger = logging.getLogger('server')
 
 bot = Bot(TOKEN)
-dispatcher = Dispatcher(bot, None, workers=0)
+update_queue = Queue()
+dispatcher = Dispatcher(bot, update_queue)
+thread = Thread(target=dispatcher.start, name='dispatcher')
+
 app = Flask('echo')
+
+chat_to_photos = {}
 
 def start(update: Update, _: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
@@ -30,14 +40,37 @@ def help_command(update: Update, _: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
-
 def echo_text(update: Update, _: CallbackContext) -> None:
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 def echo_photo(update: Update, _: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_photo(update.message.photo[-1].file_id)
+    keyboard = [
+        [InlineKeyboardButton("Yes!", callback_data='1')],
+        [InlineKeyboardButton("No!", callback_data='2')],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    chat_to_photos[update.message.chat.id] = {'photo':update.message.photo[-1].file_id, 
+                                              'send':False}
+    update.message.reply_text('Do you want to back photo?', reply_markup=reply_markup)
+
+def button(update: Update, _: CallbackContext) -> None:
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    if query.data == '1' and chat_to_photos[query.message.chat.id]['send'] is False:
+        query.message.reply_text('Get it!')
+        query.message.reply_photo(chat_to_photos[query.message.chat.id]['photo'])
+    elif query.data == '2' and chat_to_photos[query.message.chat.id]['send'] is False:
+        query.message.reply_text('As you know.')
+    
+    chat_to_photos[query.message.chat.id]['send'] = True
 
 def wrong_data(update: Update, _: CallbackContext) -> None:
     """Echo the user message."""
@@ -47,18 +80,24 @@ def wrong_data(update: Update, _: CallbackContext) -> None:
 def echo():
     logger.info(request.get_json())
     update = Update.de_json(request.json, bot)
-    dispatcher.process_update(update)
+    # dispatcher.process_update(update)
+    update_queue.put(update)
     return {'ok':True}
 
 def set_handlers():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text, echo_text))    
-    dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))    
-    dispatcher.add_handler(MessageHandler(~(Filters.photo | Filters.text), wrong_data))    
+    dispatcher.add_handler(CallbackQueryHandler(button))
+
+    dispatcher.add_handler(MessageHandler(Filters.text, echo_text))
+    dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))
+    dispatcher.add_handler(MessageHandler(~(Filters.photo | Filters.text), wrong_data))
 
 if __name__ == '__main__':
     set_handlers()
+
+    thread.start()
+
     bot.setWebhook(f'{sys.argv[1]}/{TOKEN}')
     serve(app, host='0.0.0.0', port='5000')
